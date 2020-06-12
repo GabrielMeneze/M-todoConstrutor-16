532c830dead439433925e504562473f250030029
Gabrieldevs77 <petollolmel@gmail.com>
2020-06-12 09:55:07 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace M_todoConstrutor_16
+{
+    class Program : Produto
+    {
+        static void Main(string[] args)
+        // instancia com o metodo sonstritor sem argumentos
+        {
+            Produto tenis = new Produto(90854332, "Air max 97", "Parte de cima em couro, material sintético e tecido", 237);
+            System.Console.Write("Código do tênis: ");
+            System.Console.WriteLine(tenis.Codigo);
+            System.Console.Write("Nome do tênis: ");
+            System.Console.WriteLine(tenis.Nome);
+            System.Console.Write("Descrição: ");
+            System.Console.WriteLine(tenis.Descricao);
+            System.Console.Write("Quantidade de Produtos em estoque: ");
+            System.Console.WriteLine(tenis.Estoque);
+           
+
+
+        }
+    }
+}
